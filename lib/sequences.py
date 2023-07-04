7f253c7e1351bd67b143bdd8475587d6cde45327
William Wong <william.wai.lim.wong@gmail.com>
2023-07-04 12:57:58 -0400

    completes sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = [0, 1, 1]
+    if length <= 3:
+        print(my_list[0:length])
+    else:
+        for i in range(3, length):
+            my_list.append(my_list[i-1]+my_list[i-2])
+
+        print(my_list[0:length])
+    
